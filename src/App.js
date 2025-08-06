7bf833289b8189f0aa5a583f0d4895ba96445444
Ashwin V <ashwin.ind219@gmail.com>
2025-08-06 19:56:45 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <main classclass='text-lg text-red-500 font-bold'>
+      React app
+    </main>
+  );
+}
+
+export default App;
